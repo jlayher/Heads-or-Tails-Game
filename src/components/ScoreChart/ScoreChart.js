1216b6d7f5f1b54939ebd894d24d1a02d4213353
John Layher <johnevanlayher@gmail.com>
2023-07-03 01:52:40 -0400

    rewrote ScoreChart text.  VH updates to body.

--- src/components/ScoreChart/ScoreChart.js
+++ src/components/ScoreChart/ScoreChart.js
@@ -1,11 +1,14 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState, useEffect } from "react";
 import "./scorechart.css";
 
-const ScoreChart = ({ playerOneScore, playerTwoScore, setOpenModal, openModal, player1Name, player2Name }) => {
-
-
-
-
+const ScoreChart = ({
+  playerOneScore,
+  playerTwoScore,
+  setOpenModal,
+  openModal,
+  player1Name,
+  player2Name,
+}) => {
   //whos in first
   const calcCurrentWinner = () => {
     if (playerOneScore > playerTwoScore) {
@@ -30,7 +33,9 @@ const ScoreChart = ({ playerOneScore, playerTwoScore, setOpenModal, openModal, p
             <div>Player 1</div>
             <div>Player 2</div> */}
 
-            <div className="score_container-playerbox_current">Current Winner</div>
+            <div className="score_container-playerbox_current">
+              Current Winner
+            </div>
             <div>{calcCurrentWinner()}</div>
             <br />
             <br />
@@ -38,19 +43,35 @@ const ScoreChart = ({ playerOneScore, playerTwoScore, setOpenModal, openModal, p
             <div>${Math.abs(playerOneScore)}</div>
             <br />
             <br />
-            
           </div>
-
-          
         </div>
         <div className="score_container_explain">
+          <div>
+            This application allows users to:
+            <br />
+            -Place a monetary bet.
+            <br />
+            -Guess the outcome of a coin flip.
+            <br />
+            -Calculate the current winner, and money owed.
+            <br/>
+            -Review previous game outcomes by scrolling down the "Flip Log" Panel.
+            <br />
+            <br />
+            <br />
             <div>
-              This application allows users to place a monetary bet, a guess as
-              to the outcome of the coin flip, manages a log of the flip
-              history, and displays the current winner and money owed! To play,
-              place your bet, select and submit your guess, click "Flip!", and
-              click "Start Next Game"!
+              How To Play,<br/><br/>
+              1. Player 1 places the first bet!
+Select the amount you wish to bet by hovering over the "Place Your Bet!" box, and using the up/down arrows that appear, or by entering a number 
+              using your keyboard.<br/><br/>
+
+              2. Select your Guess by clicking either Heads or Tails.<br/><br/>
+              3. Submit your Guess by clicking the "Submit Your Guess!" Button.<br/><br/>
+              4. Click the "Flip!" Button to see the Outcome!<br/><br/>
+              5. Click "Start Next Game" to Play Again, but with Player 2 choosing their guess!
+              &nbsp;The Application will track who is Winning and by How Much for You!!<br/><br/>
             </div>
+          </div>
         </div>
       </div>
     </>
